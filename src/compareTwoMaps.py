d0d4dbc6f665f78bb9e51fedee33559b75e371ab
MNandor <mnandor@users.noreply.github.com>
2022-06-09 18:00:03 +0300

    Better comparison with colors

--- src/compareTwoMaps.py
+++ src/compareTwoMaps.py
@@ -2,6 +2,15 @@
 
 from deformatter import *
 
+
+# red = '\033[91m'
+normal = '\033[0m'
+green = '\033[92m'
+yellow = '\033[93m'
+blue = '\033[94m'
+pink = '\033[95m'
+teal = '\033[96m'
+
 # We assume 1st is Joplin and 2nd is Anki for now. 
 def compareTwoMaps(joplinorg, ankiorg):
 
@@ -21,20 +30,40 @@ def compareTwoMaps(joplinorg, ankiorg):
 	for item in anki:
 		if item not in common:
 			aonly += [item]
+
+	print('\n'*5+'Common')
+	print(yellow, 'Comparison', normal)
+	print(blue, 'Joplin', normal)
+	print(teal, 'Anki', normal)
 	
 	print('\n'*5+'Common')
 	for item in common:
+		print(yellow, item, normal)
+
 		ind = joplin.index(item) 
 		orgitem = joplinorg[ind]
-		print(orgitem['j2aorgnum'], orgitem['j2aorgline'])
-		print(item)
+		print(blue, orgitem['j2aorgnum'], orgitem['j2aorgline'], normal)
+
+		ind = anki.index(item)
+		orgitem = ankiorg[ind]
+		print(teal, orgitem, normal)
+		print()
+
 
 	print('\n'*5+'Joplin Only')
 	for item in jonly: 
+		print(yellow, item, normal)
+
 		ind = joplin.index(item) 
 		orgitem = joplinorg[ind]
-		print(orgitem['j2aorgnum'], orgitem['j2aorgline'])
-		print(item)
+		print(blue, orgitem['j2aorgnum'], orgitem['j2aorgline'], normal)
+		print()
 
 	print('\n'*5+'Anki Only')
-	for item in aonly: print(item)
+	for item in aonly:
+		print(yellow, item, normal)
+
+		ind = anki.index(item)
+		orgitem = ankiorg[ind]
+		print(teal, orgitem, normal)
+		print()
