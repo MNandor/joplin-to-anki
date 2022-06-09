b5e5b392008c830b8a75acdbe91745bc34434856
MNandor <mnandor@users.noreply.github.com>
2022-06-09 16:09:19 +0300

    Display original Joplin line during comparison

--- src/compareTwoMaps.py
+++ src/compareTwoMaps.py
@@ -5,8 +5,6 @@ from deformatter import *
 # We assume 1st is Joplin and 2nd is Anki for now. 
 def compareTwoMaps(joplinorg, ankiorg):
 
-	print(joplinorg)
-
 	joplin = [deformatItem(x) for x in joplinorg]
 	anki = [deformatItem(x) for x in ankiorg]
 
@@ -25,10 +23,18 @@ def compareTwoMaps(joplinorg, ankiorg):
 			aonly += [item]
 	
 	print('\n'*5+'Common')
-	for item in common: print(item)
+	for item in common:
+		ind = joplin.index(item) 
+		orgitem = joplinorg[ind]
+		print(orgitem['j2aorgnum'], orgitem['j2aorgline'])
+		print(item)
 
 	print('\n'*5+'Joplin Only')
-	for item in jonly: print(item)
+	for item in jonly: 
+		ind = joplin.index(item) 
+		orgitem = joplinorg[ind]
+		print(orgitem['j2aorgnum'], orgitem['j2aorgline'])
+		print(item)
 
 	print('\n'*5+'Anki Only')
 	for item in aonly: print(item)
