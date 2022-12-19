965654ed758bfeeb958184d749d90bc65add1c28
MNandor <mnandor@users.noreply.github.com>
2022-12-19 02:04:46 +0200

    Support notebooks, not just notes

--- src/joplinToMarkdown.py
+++ src/joplinToMarkdown.py
@@ -18,13 +18,29 @@ def joplinToMarkdown(note):
 	cur.execute('select title, body from notes where id = ?', (note,))
 	res = cur.fetchall()
 
-	if res == []:
-		print(f'Error: missing note {note}')
-		return None
-	
-	title = res[0][0]
-	body = res[0][1]
+	if res != []: # note
+		title = res[0][0]
+		body = res[0][1]
 
-	return (title, body.split('\n'))
+		return (title, body.split('\n'))
+
+	else: # notebook
+		cur.execute('select title from folders where id = ?', (note,))
+		bigtitle = cur.fetchone()[0]
+
+		bodies = ''
+
+		cur.execute('select title, body from notes where parent_id = ?', (note,))
+		res = cur.fetchall()
+
+		for note in res:
+			title = note[0]
+			body = note[1]
+
+			bodies += '# '+title+'\n\n'
+			bodies += body+'\n\n'
+
+		return (bigtitle, bodies.split('\n'))
+	
 
 
